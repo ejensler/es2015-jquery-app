251b724de3b61e37ce83f7514c36778a9d8c9878
Ethan Ensler <ejensler@gmail.com>
2016-10-02 00:51:03 -0700

    Basic implementation using React-like components and pure render functions.
    Basic css for now, no animations.

--- public/app.js
+++ public/app.js
@@ -25,15 +25,119 @@ class Header {
   }
 
   render() {
-    return _.template(`
+    return $(_.template(`
       <header>
         <h1><%= title %></h1>
       </header>
-    `)(this);
+    `)(this));
   }
 }
 
-const header = new Header().render();
-$('#root').html(header);
+class SearchResult {
+  render(result) {
+    return $(_.template(`
+      <div>
+        <img src="<%= images.length > 0 ? images[1].url : '' %>" />
+        <span><%= name %></span>
+      </div>
+    `)(result));
+  }
+}
+
+class SearchResults {
+  render(results) {
+    let searchResults = results.map(result => {
+      return new SearchResult().render(result);
+    }).reduce((prev, next) => {
+      return prev.add(next);
+    }, $());
+    return $('<div class="search-results"></div>').append(searchResults);
+  }
+}
+
+class ArtistSearchBox {
+  constructor() {
+    this.endpoint = 'https://api.spotify.com/v1/search';
+    this.state = {
+      searchText: '',
+      results: []
+    }
+    this.SearchResults = new SearchResults();
+    this.input = null;
+  }
+
+  searchApi() {
+    const self = this;
+    $.get({
+      url: this.endpoint,
+      data: {
+        q: self.state.searchText,
+        type: 'artist',
+        limit: 6
+      },
+      dataType: 'json',
+      success: self.onGetSuccess.bind(self),
+      error: self.onGetError.bind(self)
+    });
+  }
+
+  onGetSuccess(data) {
+    console.log(`GET success ${ new Date().toLocaleString() }`)
+    this.state.results = data.artists.items;
+    appRerender();
+  }
+
+  onGetError(res) {
+    throw new Error(`error on ${this.state.endpoint} GET: ${res}`);
+  }
+
+  setupStateChangeHandlers(template) {
+    this.input = template.find('input');
+    let self = this;
+    function onKeyup() {
+      self.state.searchText = $(this).val();
+      if (self.state.searchText.length > 0) {
+        self.searchApi();
+      }
+    }
+    this.input.keyup(_.debounce(onKeyup, 300));
+    return template;
+  }
+
+  setInputFocus() {
+    this.input.focus();
+    const textLength = this.input.val().length;
+    this.input[0].setSelectionRange(textLength, textLength);
+  }
+
+  render() {
+    const searchResults = this.SearchResults.render(this.state.results);
+    let template = $(_.template(`
+      <section class="artist-search-container">
+        <input autofocus=true
+          type="text"
+          placeholder="e.g. Drake, Tame Impala, etc."
+          value="<%= state.searchText %>" />
+      </section>
+    `)(this));
+    template = this.setupStateChangeHandlers(template);
+    return template.add(searchResults);
+  }
+}
+
+class App {
+  constructor() {
+    this.Header = new Header();
+    this.ArtistSearchBox = new ArtistSearchBox();
+  }
+  render() {
+    $('#app-root').html(this.Header.render().add(this.ArtistSearchBox.render()));
+    this.ArtistSearchBox.setInputFocus();
+  }
+}
+
+const app = new App();
+const appRerender = app.render.bind(app);
+appRerender();
 
 });
